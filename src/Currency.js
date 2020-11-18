5b5db9fc03f7183798e727b07feb11b95bfec2cc
enriquetecfan <enriquemondejar15@gmail.com>
2020-11-18 16:10:12 +0100

    Change Fonts and Cards

--- src/Currency.js
+++ src/Currency.js
@@ -7,39 +7,32 @@ export default function Currency({ cur }) {
 
   return (
     <div className="card text-center shadow">
+      <h5 className="card-title py-1">
+        {cur.name}{" "}
+        {cur.symbol}{" "}
+      </h5>
       <div className="mx-auto py-2">
         <img
           src={cur.iconUrl}
           alt={cur.name}
-          width="48"
-          height="48"
+          width="50"
+          height="50"
           className="img-fluid rounded"
         />
-        <h5 className="card-title py-1" style={color}>
-          {cur.name}{" "}
-          <span class="badge badge-pill badge-primary px-2">{cur.symbol}</span>{" "}
-        </h5>
         <div className="card-body">
-          <div className="card-text">
-            Markets: <code>{cur.numberOfMarkets}</code>
+        <div className="card-text">
+            Precio: <code>${Math.floor(cur.price)}</code>
           </div>
           <div className="card-text">
-            Exchanges: <code>{cur.numberOfExchanges}</code>
+            Precio Mas Alto: <code>{cur.numberOfMarkets}</code>
           </div>
           <div className="card-text">
-            present price: <code>${Math.floor(cur.price)}</code>
+            Intercambios por min: <code>{cur.numberOfExchanges}</code>
           </div>
           <div className="card-text">
-            all time high: <code>${Math.floor(cur.allTimeHigh.price)}</code>
+            Subida ma alta en todo el tiempo: <code>${Math.floor(cur.allTimeHigh.price)}</code>
           </div>
         </div>
-        <a
-          href={cur.websiteUrl}
-          className="btn btn-primary btn-sm mb-2"
-      
-          target="_blank">
-          view
-        </a>
       </div>
     </div>
   );
